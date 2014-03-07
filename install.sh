280454c8bd5bb8fb0b05e0cb6d43c861ca2b8de9
Kenneth Perry (thothonegan) <thothonegan@gmail.com>
2014-03-06 20:21:32 -0600

    Minor cleanups to install

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-cp create_folder_from_selected.desktop `kde4-config --localprefix`/share/kde4/services/ServiceMenus/
+dir=`kde4-config --localprefix`/share/kde4/services/ServiceMenus/
+echo ">> Installing to $dir"
+
+cp create_folder_from_selected.desktop "$dir"
 kbuildsycoca4
 
