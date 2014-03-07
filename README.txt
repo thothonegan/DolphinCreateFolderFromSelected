b79ae4484ef76b60d95e764dfcbe7b968e323c6c
Kenneth Perry (thothonegan) <thothonegan@gmail.com>
2014-03-06 20:19:08 -0600

    Initial import

---
+++ README.txt
@@ -0,0 +1,1 @@
+Simple dolphin/konqueror service menu to create a folder from a selected set of files.
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp create_folder_from_selected.desktop `kde4-config --localprefix`/share/kde4/services/ServiceMenus/
+kbuildsycoca4
+
